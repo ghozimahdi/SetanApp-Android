5c03d96feee34299fe46de1e69e5079a0eaa9ded
ghozimahdi <profghozimahdi@gmail.com>
2020-05-30 15:00:56 +0700

    test

--- app/src/main/java/com/blank/setanapps/MyReceiver.kt
+++ app/src/main/java/com/blank/setanapps/MyReceiver.kt
@@ -3,10 +3,15 @@ package com.blank.setanapps
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val svc = Intent(context, MainService::class.java)
-        context?.startService(svc)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context?.startForegroundService(intent)
+        } else {
+            context?.startService(svc)
+        }
     }
 }
